9711dd9c4263d114f9a167882ca9c2d976c3f780
bsj2061 <41997990+bsj2061@users.noreply.github.com>
2023-09-21 17:45:56 +0900

    Update and rename main.py to tone.py

---
+++ code/tone.py
@@ -0,0 +1,10 @@
+import personal_color
+import cv2
+
+
+class Tone:
+  def __init__(self, file):
+    self.file = file
+
+  def analysis_tone():
+    personal_color_ = personal_color.analysis(self.file) 
